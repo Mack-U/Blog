b411076910b735f272154d9ca1e7bc71fb6cef16
Mack-U <76881123+Mack-U@users.noreply.github.com>
2021-08-21 17:42:44 -0500

    commit adding css

--- sanity-react/src/components/AllPosts.js
+++ sanity-react/src/components/AllPosts.js
@@ -29,9 +29,10 @@ export default function AllPosts() {
 
   return (
     <div>
-      <h2 class="title">Basics for Beginners</h2>
-      <h3 class="intro">Welcome to my blog! This is a blog that targets to help out beginners who want to be Front-End Developers. I hope that I will be able to take small concepts and break them down in a way that anybody can understand it. We will be diving into HTML, CSS, and JavaScript concepts.</h3>
-      <div>
+      <h2 class="title">Self-Developing</h2>
+      <h3 class="intro">Personal Blog by Donnie Underwood</h3>
+      <h3 class="intro">Welcome to my blog! This is a blog that targets to help out beginners who want to be front-end developers. I hope that I will be able to take small concepts and break them down in a way that anybody can understand it. We will be diving into HTML, CSS, and JavaScript concepts. There will also be articles in which we dive into the psychology of front-end development and how you can use it to be a more effective developer.</h3>
+      <div class ="post">
         {allPostsData &&
           allPostsData.map((post, index) => (
             <Link to={"/" + post.slug.current} key={post.slug.current}>
