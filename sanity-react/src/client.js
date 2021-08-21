341a07873d7155e1c1a9d29a7ec41235ea0b0aa1
Mack-U <76881123+Mack-U@users.noreply.github.com>
2021-08-20 23:15:20 -0500

    commit fixing all of the issues within sanity and have begin the initial styling

--- sanity-react/src/client.js
+++ sanity-react/src/client.js
@@ -3,5 +3,6 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "y930kxrm", // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
+  apiVersion: '2021-08-20',
   useCdn: true,
 });
--- sanity-react/src/components/AllPosts.js
+++ sanity-react/src/components/AllPosts.js
@@ -1,7 +1,49 @@
 // src/components/AllPosts.js
 
-import React from "react";
+// src/components/AllPosts.js
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
 
 export default function AllPosts() {
-  return <h2>AllPosts Page</h2>;
+  const [allPostsData, setAllPosts] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+          _id,
+          url
+        }
+      }
+    }`
+      )
+      .then((data) => setAllPosts(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h2 class="title">Basics for Beginners</h2>
+      <h3 class="intro">Welcome to my blog! This is a blog that targets to help out beginners who want to be Front-End Developers. I hope that I will be able to take small concepts and break them down in a way that anybody can understand it. We will be diving into HTML, CSS, and JavaScript concepts.</h3>
+      <div>
+        {allPostsData &&
+          allPostsData.map((post, index) => (
+            <Link to={"/" + post.slug.current} key={post.slug.current}>
+              <span key={index}>
+                <img class="picture" src={post.mainImage.asset.url} alt="" />
+                <span>
+                  <h2>{post.title}</h2>
+                </span>
+              </span>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
 }
